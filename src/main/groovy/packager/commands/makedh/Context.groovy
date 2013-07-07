9fbd0212d1dc5a3ab0328699545970217235677a
Tobias Gesellchen <tobias@gesellix.de>
2013-07-07 18:31:33 +0200

    make plugin compatible with gradle 1.6

--- src/main/groovy/packager/commands/makedh/Context.groovy
+++ src/main/groovy/packager/commands/makedh/Context.groovy
@@ -15,27 +15,27 @@
  */
 package packager.commands.makedh
 
-import static org.apache.commons.lang.StringUtils.*
+import static org.apache.commons.lang.StringUtils.capitalize
 
-@Immutable
+@groovy.transform.Immutable
 final class Context {
 
-    String name
-    String version
-    String releaseNotes
-    String author
-    String email
-    String homepage
-    String time
-    String description
-    SortedSet<String> dirs
-    SortedSet<String> depends
+  String name
+  String version
+  String releaseNotes
+  String author
+  String email
+  String homepage
+  String time
+  String description
+  SortedSet<String> dirs
+  SortedSet<String> depends
 
-    def asType(Class type) {
-        "to${capitalise(type.simpleName)}"()
-    }
+  def asType(Class type) {
+    "to${capitalize(type.simpleName)}"()
+  }
 
-    Map toMap() {
-        [name:name, version:version, releaseNotes:releaseNotes, author:author, email:email, time:time, dirs:dirs, homepage:homepage, description:description, depends:depends]
-    }
+  Map toMap() {
+    [name: name, version: version, releaseNotes: releaseNotes, author: author, email: email, time: time, dirs: dirs, homepage: homepage, description: description, depends: depends]
+  }
 }
